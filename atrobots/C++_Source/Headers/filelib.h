2f6a3089313602296ac903161c251b9d3f4adcad
Mitch Z <destructoglitch@gmail.com>
2018-01-30 14:25:27 -0500

    Finished myfile.cc

--- atrobots/C++_Source/Headers/filelib.h
+++ atrobots/C++_Source/Headers/filelib.h
@@ -10,10 +10,11 @@
 #include <cstdio>
 #include <cstdlib>
 
+using namespace std;
+
 unsigned short	textattr;
 string		workstr;
 
-#endif
 
 /* wtf is this for?
   basex : byte = 1;
@@ -23,3 +24,20 @@ string		workstr;
   endx  : byte = 24;
   endy  : byte = 80;
 */
+
+extern string addfront(string, short);
+extern string addrear(string, short);
+extern string copy(string, short, short);
+extern string lstr( string, short);
+extern string rstr(string, short);
+extern bool exist(string);
+extern bool valid(string);
+extern string name_form(string);
+extern string exten(string name);
+extern string base_name(string);
+extern string attribs(unsigned char);
+extern string path(string fn) {
+extern string no_path(string);
+extern short file_length(string);
+
+#endif
--- atrobots/C++_Source/myfile.cc
+++ atrobots/C++_Source/myfile.cc
@@ -30,213 +30,34 @@ void rename_file(string fn, string fn2) {
 	std::rename(f.c_str(), fn2.c_str());
 }
 
-void open_filein(unsigned short h, string fn) {
-	unsigned short w;
-
-	the_name = fn + "\0";
-
-    __asm__("push %eds"
-            "mov  %dx,      seg the_name"
-            "mov  %ds,      %dx"
-            "mov  %dx,      offset the_name"
-            "inc  %dx"
-            "mov  %ax,      %3D00h;"
-            "int  %21h"
-            "pop  %ds"
-            "mov  w,       %ax");
-
-	h = w;
-}
-
-void open_fileboth(unsigned short h, string fn) {
-	unsigned short w;
-
-	the_name = fn + "\0";
-
-	__asm__("push  %ds"
-		"mov   %dx,     seg the_name"
-		"mov   %ds,     %dx"
-		"mov   %dx,     offset the_name"
-		"inc   %dx"
-		"mov   %ax,     %3D02h;"
-		"int  %21h"
-		"pop   %ds"
-		"mov   w,      %ax");
-
-	h = w;
-}
-
-void open_fileout(unsigned short h, string fn) {
-	unsigned short w;
-
-	the_name =fn + "\0";
-
-	if ( exist( lstr(fn, fn.length()-1) ) ) {
-		__asm__("push  %ds"
-			"mov   %dx,     seg the_name"
-			"mov   %ds,     %dx"
-			"mov   %dx,     offset the_name"
-			"inc   %dx"
-			"mov   %ax,     %3D01h;"
-			"int   %21h"
-			"pop   %ds"
-			"mov   w,      %ax");
-
-	} else {
-		__asm__("push  %ds"
-			"mov   %dx,     seg the_name"
-			"mov   %ds,     %dx"
-			"mov   %dx,     offset the_name"
-			"inc   %dx"
-			"mov   %cx,     %20h"
-			"mov   %ax,     %3C00h;"
-			"int   %21h"
-			"pop   %ds"
-			"mov   w,      %ax");
-	}
-	h = w;
-}
-
-void create_fileout(unsigned short h; string fn);
-	unsigned short w;
-
-	the_name = fn + "\0";
-
-	__asm__("push  %ds"
-		"mov   %dx,     seg the_name"
-		"mov   %ds,     %dx"
-		"mov   %dx,     offset the_name"
-		"inc   %dx"
-		"mov   %cx,     %20h"
-		"mov   %ax,     %3C00h;"
-		"int   %21h"
-		"pop   %ds"
-		"mov   w,      %ax");
-
-	h = w;
-}
-
-
 // Original function was read_file(unsigned short h, void * ploc, short len)
+// H is file address, len is how many bytes to read, ploc is unused(?)
 
-void read_file(unsigned short h, void * ploc, short len) {
-	unsigned short tseg, tofs, pp, w;
-	short ll;
+void read_file(string name, short byte_len) {
+	fstream file(name, fstream::in | fstream::bin); // Read file in binary mode
 
-	ll = len; w = 0;
+	if ( file.good() ) {
+		char *buff[byte_len];
 
-	__asm__("push  %ds"
-		"mov   %bx,     h"
-		"mov   %cx,     ll"
-		"mov   %dx,     tseg"
-		"mov   %ds,     %dx"
-		"mov   %dx,     tofs"
-		"mov   %ax,     %3F00h;"
-		"int   %21h"
-		"jc    uh_oh"
-		"jmp   alright"
-		"uh_oh:"
-		"mov   w,      %ax"
-		"alright:"
-		"pop   %ds"
-		"cmp   ll,     %ax"
-		"je    ok"
-		"mov   %the_eof, $1"
-		"ok:"
-		"mov   ll,     %ax");
+		file.read(buff, byte_len);
 
-	len = ll;
-
-	if ( w != 0)
-		printf(" ****** " w " ****** \n");
+		file.close();
+	}
 }
 
 // data type word == unsigned short
 // data type longint == int
 
-Procedure write_file(h:word; ploc:pointer; var len:integer);
-var
- tseg,tofs:word;
- ll:integer;
-begin
- tseg:=seg(ploc^);
- tofs:=ofs(ploc^);
- ll:=len;
-    __asm__("push  ds"
+void write_file(unsigned short name, string text);
+	fstream file(name, fstream::out|fstream::bin);
 
-            "mov   %bx,     h"
-            "mov   %cx,     ll"
-            "mov   %dx,     tseg"
-            "mov   %ds,     %dx"
-            "mov   %dx,     tofs"
-            "mov   %ax,     %4000h;"
-            "int   %21h"
-            "pop   %ds"
-            "mov   ll,     %ax");
- len:=ll;
-end;
+	if ( file.good() ) {
+		text >> file;
+		file.close();
+	}
+}
 
-Procedure read_long(h:word; ploc:pointer; var len:word);
-var
- tseg,tofs,pp,w:word;
- ll:word;
-begin
- tseg:=seg(ploc^);
- tofs:=ofs(ploc^);
- ll:=len; w:=0;
-
-    __asm__("push  %ds"
-            "mov   %bx,     h"
-            "mov   %cx,     ll"
-            "mov   %dx,     tseg"
-            "mov   %ds,     %dx"
-            "mov   %dx,     tofs"
-            "mov   %ax,     %3F00h;"
-            "int   %21h"
-            "jc    uh_oh"
-            "jmp   alright"
-            "uh_oh:"
-            "mov   %w,      %ax"
-            "alright:"
-            "pop   %ds"
-            "cmp   ll,     %ax"
-            "je    ok"
-            "mov   %the_eof,$1"
-            "ok:"
-            "mov   ll,     %ax"
- len:=ll;
- if w<>0 then begin writeln(' ****** ',w,' ****** '); {pausescr;} end;
-end;
-
-Procedure write_long(h:word; ploc:pointer; var len:word);
-var
- tseg,tofs:word;
- ll:word;
-begin
- tseg:=seg(ploc^);
- tofs:=ofs(ploc^);
- ll:=len;
-    __asm__("push  %ds"
-            "mov   %bx,     h"
-            "mov   %cx,     ll"
-            "mov   %dx,     tseg"
-            "mov   %ds,     %dx"
-            "mov   %dx,     tofs"
-            "mov   %ax,     %4000h;"
-            "int   %21h"
-            "pop   %ds"
-            "mov   ll,     %ax");
- len:=ll;
-end;
-
-procedure close_file(h:word);
-begin
-    __asm__("mov   %ax,     %3E00h"
-            "mov   %bx,     h"
-            "int   %21h");
- end;
-end;
-
+/*	Likely unneeded at this point in time
 Procedure file_seek(h,d:word; m:byte; var p:word);
 var
  w:word;
@@ -271,61 +92,4 @@ begin
  p:=d2; p:=p shl 16;
  p:=p or d1;
 end;
-
-{procedure read_long(var h:word; p:pointer; var l:longint);
-var
- len:longint;
- k:integer;
-begin
- l:=len;
- if len<=0 then exit;
- if len>65535 then len:=65535;
- if len<=32000 then
-  begin
-   k:=len;
-   read_file(h,p,k);
-   l:=k;
-  end
- else
-  begin
-   k:=32000;
-   read_file(h,p,k);
-   if k<32000 then l:=k else
-    begin
-     k:=len-32000;
-     read_file(h,ptr(seg(p^),ofs(p^)+32000),k);
-     l:=k+32000;
-    end;
-  end;
-end;
-
-procedure write_long(var h:word; p:pointer; var l:longint);
-var
- len:longint;
- k:integer;
-begin
- l:=len;
- if len<=0 then exit;
- if len>65535 then len:=65535;
- if len<=32000 then
-  begin
-   k:=len;
-   write_file(h,p,k);
-   l:=k;
-  end
- else
-  begin
-   k:=32000;
-   write_file(h,p,k);
-   if k<32000 then l:=k else
-    begin
-     k:=len-32000;
-     write_file(h,ptr(seg(p^),ofs(p^)+32000),k);
-     l:=k+32000;
-    end;
-  end;
-end;}
-
-begin
- the_eof:=0;
-end.
+*/
