560919ce26a90990ed1ab3ab22f64b8a400c0852
Gerald <wolffg@sunyit.edu>
2018-02-28 21:13:24 -0500

    Some bug fixes: -Got an actual window to show up (shows arena and health are -Invlaid robots don't work -Displays robots as a colored line -side bar has boxes same color as robots
    Attempting to run with robots forces unlimited loop so far

--- atrobots/C++_Source_Gerald/Headers/myfile.h
+++ atrobots/C++_Source_Gerald/Headers/myfile.h
@@ -1,16 +1,37 @@
 #ifndef MYFILE_H
 #define MYFILE_H
 
+#include "filelib.h"
+
+#include <fstream>
+#include <iostream>
 #include <string>
+#include <cstdint>
 
-std::string the_name;
-unsigned char the_eof;
 
-extern std::string lstr(std::string, unsigned short);
-extern int file_size(std::string);
-extern void delete_file(std::string);
-extern void rename_file(std::string, std::string);
-extern void read_file(std::string, unsigned short);
-extern void write_file(std::string, std::string);
+using namespace std;
+
+extern string	the_name;
+extern uint8_t	the_eof;
+
+
+extern int32_t	file_size(string *);
+extern void		delete_file(string *);
+extern void		rename_file(string *, string *);
+extern string	lstr(string *, int32_t);
+
+extern void		open_filein(FILE *, string *);
+extern void		open_fileout(FILE *, string *);
+extern void		open_fileboth(FILE *, string *);
+extern void		create_fileout(FILE *, string *);
+
+extern void		read_file(FILE *, void *, int16_t *);
+extern void		write_file(FILE *, void *, int16_t *);
+extern void		read_long(FILE *, void *, uint16_t *);
+extern void		write_long(FILE *, void *, uint16_t *);
+extern void		close_file(FILE *);
+
+extern void		file_seek(FILE *, uint16_t, uint8_t, FILE *);
+extern void		long_seek(FILE *, int, uint8_t, FILE *);
 
 #endif
--- atrobots/C++_Source_Gerald/myfile.cc
+++ atrobots/C++_Source_Gerald/myfile.cc
@@ -1,60 +1,118 @@
 #include "Headers/myfile.h"
-#include "Headers/filelib.h"
 
-using namespace std;
+string	the_name;
+uint8_t the_eof;
 
-
-string lstr(string s1, unsigned short l) {
-	if ( s1.length() <= l )
-		return(s1);
-	else
-		return( copy(s1,1,l) );
+string lstr(string s1, int32_t l){
+	if (s1.length() <= 1) {
+		return s1;
+	} else {
+		return copy(s1, 1, l);
+	}
 }
 
-int file_size(string fn) {
-	if ( !exist(fn) )
-		return(-1);
+int32_t file_size(string fn){
+	FILE * f;
+	int32_t size;
 
-	return(file_length(fn));
+	if( !exist(fn) ) {
+		return -1;
+	} else {
+		f =fopen(fn.c_str(), "r");
+		if ( f == NULL )
+			return -1;
+		else {
+			fseek(f, 0L, SEEK_END);
+			size = ftell(f);
+			fclose(f);
+			return size;
+		}
+	}
+
 }
+
 
 void delete_file(string fn) {
-	if ( !exist(fn) )
-		return;
-
-	remove(fn.c_str());
+	if ( exist(fn) ) {
+		remove(fn.c_str());
+	}
 }
+
 
 void rename_file(string fn, string fn2) {
-	if ( !exist(fn) )
-		return;
-
-	std::rename(fn.c_str(), fn2.c_str());
+	if ( exist(fn) )
+		rename(fn.c_str(), fn2.c_str());
 }
 
-// Original function was read_file(unsigned short h, void * ploc, short len)
-// H is file address, len is how many bytes to read, ploc is unused(?)
 
-void read_file(string name, short byte_len) {
-	fstream file(name, fstream::in | fstream::binary); // Read file in binary mode
+// Changed param 1 from word pointer (memory address) to file point32_ter
+void open_filein(FILE * fp, string fn) {
+	fp = fopen(fn.c_str(), "rb");
 
-	if ( file.good() ) {
-		char buff[byte_len];
+	the_name = fn;
+}
 
-		file.read(buff, byte_len);
+void open_fileboth(FILE * fp, string fn) {
+	the_name = fn;
 
-		file.close();
+	if ( exist(fn) ) {
+		// If exist, read/write file in binary, keeping its data
+		fp = fopen(fn.c_str(), "rb+");
+	} else {
+		// If not exist, create file and write/read file in binary
+		fp = fopen(fn.c_str(), "wb+");
 	}
 }
 
-// data type word == unsigned short
-// data type longint == int
+void open_fileout(FILE * fp, string fn) {
+	the_name = fn;
 
-void write_file(string name, string text) {
-	fstream file(name, fstream::out|fstream::binary);
+	fp = fopen(fn.c_str(), "wb");
+}
 
-	if ( file.good() ) {
-		file << text;
-		file.close();
-	}
+void create_fileout(FILE * fp, string fn) {
+	open_fileout(fp, fn);
+}
+
+
+void read_file(FILE * fp, void * ploc, int16_t *bytes) {
+	*bytes = fread(ploc, 1, *bytes, fp);
+}
+
+void write_file(FILE * fp, void * ploc, int16_t * bytes) {
+	*bytes = fwrite(ploc, 1, *bytes, fp);
+}
+
+void read_long(FILE * fp, void * ploc, uint16_t *bytes) {
+	*bytes = fread(ploc, 1, *bytes, fp);
+}
+
+void write_long(FILE * fp, void * ploc, uint16_t * bytes) {
+	*bytes = fwrite(ploc, 1, *bytes, fp);
+}
+
+void close_file(FILE * fp) {
+	fclose(fp);
+}
+
+void file_seek(FILE * h, uint16_t amount, uint8_t position, FILE * new_pos) {
+	if ( position == 0 )
+		fseek(h, amount, SEEK_SET);
+	else if ( position == 1 )
+		fseek(h, amount, SEEK_CUR);
+	else if ( position == 2 )
+		fseek(h, amount, SEEK_END);
+
+	new_pos = h;
+}
+
+void long_seek(FILE * h, int32_t amount, uint8_t position, FILE * new_pos) {
+	if ( position == 0 )
+		fseek(h, amount, SEEK_SET);
+	else if ( position == 1 )
+		fseek(h, amount, SEEK_CUR);
+	else if ( position == 2 )
+		fseek(h, amount, SEEK_END);
+
+	new_pos = h;
 }
